4c27b3487bc1562c48cfcbeb796ae6f210b3bcd6
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-09-13 19:00:52 +0100

    Update module-two-week-one.py

--- module-two-week-1.py
+++ module-two-week-1.py
@@ -1,8 +1,8 @@
-//Unsorted list, sorted
+#Step 1: Unsorted list, sorted
 num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
 num_list.sort()
 
-//Enumerate Function
+#Test: Enumerate Function
 num_index = enumerate(num_list)
 print(list(num_index))
 
@@ -14,15 +14,24 @@ for num in num_list:
 count = 0
 
 for i in num_list:
+#Step 2: Print Under 45
   if i > 45:
     print("Over 45")
+#Step 3: Print Over 45
   else:
     print("Under 45")
-          
+
+#Step 4: Enumerate Function
 for x,num in enumerate(num_list):
+#Step 5 and 6: Count +=1
   count += 1
   if num == 36:
     print('Number found at ', x)
+    #Step 8: Break when found
     break
-
+#Step 7: Print Count Value
 print(count)
+
+if 36 in num_list:
+    index = num_list.index(36)
+    print('Number found at', index + 1)
