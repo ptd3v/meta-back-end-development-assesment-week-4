00414d8f3dd3937eeee3ea53f8eed1a55039a764
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-09-13 18:42:38 +0100

    Update module-two-week-1.py

--- module-two-week-1.py
+++ module-two-week-1.py
@@ -1,5 +1,11 @@
+//Unsorted list, sorted
 num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
 num_list.sort()
+
+//Enumerate Function
+num_index = enumerate(num_list)
+print(list(num_index))
+
 print (num_list)
 
 for num in num_list:
