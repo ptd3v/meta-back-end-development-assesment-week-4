f70167f82d880bfb3b3f7ff1dfc0f47ba015e55b
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-09-14 17:28:20 +0100

    Update reuable-function-example.py

--- reuable-function-example.py
+++ reuable-function-example.py
@@ -5,3 +5,7 @@ bill = 150.00
 tax_rate = 15
 total_tax = (bill * tax_rate) / 100.00
 print ('Total Tax: ', total_tax)
+
+#Structured Function
+def calculate_bill (bill, tax_rate):
+  return (bill * tax_rate) /100.00
