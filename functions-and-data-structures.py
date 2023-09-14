07ced24f75f5268ab4d7f74c9969752cd4fa9d31
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-09-14 17:56:19 +0100

    Added variable scopes (global, local)

--- functions-and-data-structures.py
+++ functions-and-data-structures.py
@@ -1,5 +1,4 @@
 #Reuable Function Example, Adding Tax
-
 #Unstructured Function
 bill = 150.00
 tax_rate = 15
@@ -12,3 +11,18 @@ def calculate_bill (bill, tax_rate):
 
 #Note: Functions only run when they are called.
 print ('Total Tax: ',calculate_bill(250,15))
+
+#### ####
+
+#Local and Global Variables
+global_var= 10
+
+def fn1 ():
+  local_var = 20
+  print('Global Var: ', global_var, 'Local Var: ', local_var)
+
+#Reminder: Functions only run when they are called.
+fn1()
+
+#### ####
+
