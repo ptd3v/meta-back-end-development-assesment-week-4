b8584dac7e397ecd2377197d4ac973f5f7f7a7ca
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-09-15 18:45:40 +0100

    Added Lists.py

--- functions-and-data-structures.py
+++ functions-and-data-structures.py
@@ -31,3 +31,27 @@ print('Global Var: ', global_var,) #'Local Var: ', local_var, 'Enclosed Var: ', 
 #Data Strcuture Types
 #In-Built: List, Tuple, Dictionary, Set
 #User-Defined: Stack, Tree, Graph, Queue, Linked List, HashMap 
+
+#### ####
+
+#Lists
+list1 = [1, 2, 3, 4, 5]
+list2 = ['A', 'B', 'C', 'D', 'E']
+list3 = ['String', 1, 1.2, True]
+list4 = [1, [2, 3, 4], 5, 6] #Nested List
+
+print (list4[1]) #Print the value at index 1
+print(*list4, sep = ", ")
+
+list4.insert(4, 69) #Add the number 69, to position 4. Nice.
+list4.insert(len(list4), 0) #Add 0 to the end of List4. Len is to calculate the total size, ensuring it adds to the end.
+list4.extend([96, 97, 98])
+list4.append(99)
+print(*list4, sep = ", ")
+list4.pop(0) #Remove the value at index 0
+del list4[0] #Remove the new value at index 0
+print(*list4, sep = ", ")
+
+#Iterating through Lists
+for x in list4:
+    print('Value: ', x)
