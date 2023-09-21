414e533308f9fa7932d324f511bf116c958f3ed2
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-09-21 15:38:43 +0100

    Practice: IndexError Exercise

--- errors-exceptions-and-file-handling.py
+++ errors-exceptions-and-file-handling.py
@@ -16,6 +16,14 @@ except ZeroDivisionError as e:
 
 print(divide_by(10,2))
 
+# Starter code
+items = [1,2,3,4,5]
+try:
+    item = items[6]
+    print(item)
+except Exception as e:
+    print("Item does not exist in the list:", e)
+
 #### ####
 
 loyalty_customer = True
