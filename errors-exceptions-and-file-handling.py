fbce20a7a27a4b88c0fa81517a004bfe437d4608
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-09-25 19:01:59 +0100

    Exception Solutions Added

--- errors-exceptions-and-file-handling.py
+++ errors-exceptions-and-file-handling.py
@@ -16,13 +16,32 @@ except ZeroDivisionError as e:
 
 print(divide_by(10,2))
 
-# Starter code
-items = [1,2,3,4,5]
+#Solutions, Task 1 : IndexError
 try:
     item = items[6]
     print(item)
-except Exception as e:
-    print("Item does not exist in the list:", e)
+except: 
+    print("The index does not exist in the list.")
+
+#Solutions, Task 2 : ZeroDivisionError
+def divide_by(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 0
+    except Exception as e:
+        print(e, 'Something went wrong!')
+
+ans = divide_by(10,0)
+print(ans)
+
+#Solutions, Task 3 : FileNotFoundError
+try:
+    with open('file_does_not_exist.txt', 'r') as file:
+        print(file.read())
+except:
+    print("Unable to locate file")
+
 
 #### ####
 
