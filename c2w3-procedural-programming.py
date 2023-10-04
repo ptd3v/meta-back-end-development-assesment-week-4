ab9e2ee389e4f5aeb854e26ce5b7e609f9186f6c
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-10-04 15:26:17 +0100

    Efficient Coding Examples

--- c2w3-procedural-programming.py
+++ c2w3-procedural-programming.py
@@ -1,1 +1,13 @@
 #Procedural Programming: Course 2/9. Week 3/5.
+
+#Inefficient Code
+a = 1
+b = 2
+sum = a + b
+print(sum)
+
+#Efficient Code
+def sum(a, b):
+  return a + b
+print(sum(1,2))
+print(sum(3,4))
