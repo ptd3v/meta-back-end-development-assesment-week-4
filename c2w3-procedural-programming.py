7e4460e0492214e0ac6d9220fe0e4b5fe6fd58b7
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-10-04 16:04:55 +0100

    Palindrome creation

--- c2w3-procedural-programming.py
+++ c2w3-procedural-programming.py
@@ -17,3 +17,13 @@ print(sum(3,4))
 #### ####
 
 #Algorithms
+def palindrome(str):
+    startIndex = 0
+    endIndex = len(str) - 1
+
+    for x in str:
+        if str[startIndex] != str[endIndex]:
+            return False
+    return True
+
+print(palindrome('racecar'))
