7aac37d9bf14791e276aa93d4b33117820c3f033
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-10-13 17:56:40 +0100

    OOP Fundamentals Updated

--- c2w3-object-oriented-pogramming.py
+++ c2w3-object-oriented-pogramming.py
@@ -1,1 +1,13 @@
+#Object Orientated Programming (OOP)
+#Popular examples of OOP: Python, Java, C++.
+#OOP is highly modular, making code easy to understand.
 
+#Classes, objects and methods.
+#Class - Logical code block, contains attributes(variables) and behaviours(functions).
+#Objects - An instance of a class, 
+#Methods - Functions defined with a class that determine the behaviour of an instance.
+
+#4 Concepts of OOP
+#Inheritance - Creating a new class from an old one. E.g A parent class, can have a child class which is a copy.
+#Polymorphism - A single function can act differently depending on te object. E.g str+str concatenates, 1+2 adds.
+#Encapsulation - Prevents access to variables and methods by wrapping them in a class.
