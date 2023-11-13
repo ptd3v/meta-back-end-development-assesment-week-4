76ebf00dd8e901cba363e269dc3c8872b172ab76
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-11-13 14:34:30 +0000

    Importing an external module

--- c2w4-modules.py
+++ c2w4-modules.py
@@ -1,6 +1,13 @@
 #Modules
-import numpy #Will only work after numpy has been installed
+#A module can be any .py file. You cannot call other file types, such as txt or xml.
+
+import numpy #Will only work after numpy has been installed. Will work if numpy is a built in module.
 
 import sys #Importing sys to specify a path
-sys.path.insert(1, r'C:\windows\examplefolder\examplefile.py')#1 = First indexed location, r = Used when passing a location as an argument.
+sys.path.insert(1, r'C:\windows\examplefolder')#1 = First indexed location, r = Used when passing a location as an argument.
 #The sys.path list now has a new path to check for modules
+
+#Once the folder locationhas been established, a file within can be called.
+import examplefile #The file name in the location is examplefile.py
+print (examplefile.names) #Within this file, is a variable called names.
+
