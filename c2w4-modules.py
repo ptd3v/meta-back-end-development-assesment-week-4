3762556bb1afa7f3538c166912e3ff8fc4db24c8
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-11-13 14:30:25 +0000

    Importing sys module

--- c2w4-modules.py
+++ c2w4-modules.py
@@ -1,1 +1,6 @@
+#Modules
+import numpy #Will only work after numpy has been installed
 
+import sys #Importing sys to specify a path
+sys.path.insert(1, r'C:\windows\examplefolder\examplefile.py')#1 = First indexed location, r = Used when passing a location as an argument.
+#The sys.path list now has a new path to check for modules
