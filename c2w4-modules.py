ae95a9359f4a723041b4a76f4641c327b2024160
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-11-21 19:34:26 +0000

    NLTK Module

--- c2w4-modules.py
+++ c2w4-modules.py
@@ -184,3 +184,33 @@ freq         1      1
 
 ----
 
+#NLTK
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+# Print statement 1
+print(word_tokenize(text))
+# Print statement 2
+print(nltk.tokenize.sent_tokenize(text))
+
+stopwords = stopwords.words("english")
+new_text = []
+for i in text.split():
+    if i not in stopwords:
+        new_text.append(i)
+
+# Print statement 3
+print(new_text)
+
+# NLTK Output
+['Lorem', 'Ipsum', 'is', 'simply', 'dummy', 'text', 'of', 'the', 'printing', 'and', 'typesetting', 'industry', '.', 'Lorem', 'Ipsum', 'has', 'been', 'the', 'industry', "'s", 'standard', 'dummy', 'text', 'ever', 'since', 'the', '1500s', ',', 'when', 'an', 'unknown', 'printer', 'took', 'a', 'galley', 'of', 'type', 'and', 'scrambled', 'it', 'to', 'make', 'a', 'type', 'specimen', 'book', '.']
+
+['Lorem Ipsum is simply dummy text of the printing and typesetting industry.', "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."]
+
+['Lorem', 'Ipsum', 'simply', 'dummy', 'text', 'printing', 'typesetting', 'industry.', 'Lorem', 'Ipsum', "industry's", 'standard', 'dummy', 'text', 'ever', 'since', '1500s,', 'unknown', 'printer', 'took', 'galley', 'type', 'scrambled', 'make', 'type', 'specimen', 'book.']
+
+---- 
+
