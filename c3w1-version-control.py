850b221d12213291db176fa6ee35aff0cf54979c
Paul T <102877038+ptd3v@users.noreply.github.com>
2024-02-06 15:54:57 +0000

    Command Line Examples Added

--- c3w1-version-control.py
+++ c3w1-version-control.py
@@ -1,7 +1,10 @@
 #Version Control
 
-def vs:
-  #1
-  #2
-  #3
+#Command Line Command Examples:
+cd /Desktop: # Change Directory, changes the current place the cmd is looking
+mkdir example # Create a new directory called example in your current location
+cd example # Enter the newly created example folder.
+touch example.js # Does not work, Linux only?
+code example.js # Open the newly created file in VSCode.
 
+#Unix Commands
