30ff0fca79452d0873baea2eedbc9d3e7dc86ba4
Paul T <102877038+ptd3v@users.noreply.github.com>
2024-02-12 15:03:51 +0000

    Unix Redirections Part 1

--- c3w1-version-control.py
+++ c3w1-version-control.py
@@ -59,4 +59,7 @@ wc example.txt -w # Will display the total words within the .txt file.
 ls | wc -w # The outcome would be two, becuase there are two files in this folder. example.txt and example1.txt.
 cat example.txt example1.txt | wc -w # Print the total word count within these two text files.
 
-
+Three kinds of Input/ Output redirections:
+1. Input
+2. Output
+3. Error
