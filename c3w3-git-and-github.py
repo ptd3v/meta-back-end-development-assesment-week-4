d035dd9cbb4ee9f398c52e8570b04f4d383f32d7
Paul T <102877038+ptd3v@users.noreply.github.com>
2024-02-14 20:05:17 +0000

    Update c3w3-git-and-github.py

--- c3w3-git-and-github.py
+++ c3w3-git-and-github.py
@@ -1,1 +1,3 @@
+#Git is a version control system designed to help users keep track of changes to files within their projects.     
+#Git is used through the CLI (command line interface) and GitHub is a cloud platform.
 
