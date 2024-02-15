0678fc5f497e5a855eb7ad989d5ae2a30accd47a
Paul T <102877038+ptd3v@users.noreply.github.com>
2024-02-15 16:39:28 +0000

    How Git works

--- c3w3-git-and-github.py
+++
@@ -1,3 +0,0 @@
-#Git is a version control system designed to help users keep track of changes to files within their projects.     
-#Git is used through the CLI (command line interface) and GitHub is a cloud platform.
-
---
+++ c3w3-git-and-github.ts
@@ -0,0 +1,17 @@
+# Git is a version control system designed to help users keep track of changes to files within their projects.     
+# Git is used through the CLI (command line interface) and GitHub is a cloud platform.
+
+GitBash - Windows
+#Terminal - MAC
+
+ls - la #( -la = List all command)
+# The ls -al command is a combination of ls -l (use a long listing format) and ls -a (do not ignore entries starting with .)
+# In Linux, all hidden folders start with a .
+
+Git Workflow: 
+Working Directory - Modified > (Git Add) > Staging Area > (git commit) > Commited Files/ Folders > (git push) > Remote Repository
+After: Remote Repository > (git fetch) > Commited Files
+After: Remote Repository > (git checkout) > Working Directory
+
+// The correct workflow is from the working directory to the staging area, then to committed files, remote repository and lastly to your colleague.
+
