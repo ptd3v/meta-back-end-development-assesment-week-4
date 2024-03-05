b933a94786bcbed0b49e11457a1390fea2ba1038
Paul T <102877038+ptd3v@users.noreply.github.com>
2024-03-05 19:43:36 +0000

    Update c4w1-database-back-end-development.sql

--- c4w1-database-back-end-development.sql
+++ c4w1-database-back-end-development.sql
@@ -1,4 +1,3 @@
 C does not allow you to count the length of an array like Python.
 A Primary Key field contains unique values that cannot be replicated elsewhere in the table. This avoids potential confusion between tables with similarities in data.
   
-
