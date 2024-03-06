2417877eaed7c48892347047534b9d2b3e46e3c6
Paul T <102877038+ptd3v@users.noreply.github.com>
2024-03-06 17:58:14 +0000

    Update c4w1-database-back-end-development.sql

--- c4w1-database-back-end-development.sql
+++ c4w1-database-back-end-development.sql
@@ -1,3 +1,4 @@
 C does not allow you to count the length of an array like Python.
 A Primary Key field contains unique values that cannot be replicated elsewhere in the table. This avoids potential confusion between tables with similarities in data.
   
+NoSQL databases are a type of database that store data in a variety of different formats.
