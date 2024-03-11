8daebf0a27a0a06fbc26880e34e687489bffdb26
Paul T <102877038+ptd3v@users.noreply.github.com>
2024-03-11 13:55:43 +0000

    CS50 .py Update Examples

--- c4w1-database-back-end-development.js
+++ c4w1-database-back-end-development.js
@@ -36,3 +36,94 @@ average = sum(exlist) / len(exlist)
 print(average)
 print(f"Average: {average}")
 
+def main():
+    woof(4)
+
+for _ in range(4):
+    print("Eenis")
+
+def define():
+    for _ in range(2):
+        print("Define")
+
+define()
+
+for _ in range (1):
+    define()
+
+def woof(n):
+    for _ in range(n):
+        print("Woof!")
+
+main()
+
+def calculator():
+    x = int(input("Enter A: "))
+    y = int(input("Enter B: "))
+    print (x + y)
+
+calculator()
+
+exlist = [1, 3, 5, 7, 9, 11, 70]
+average = sum(exlist) / len(exlist)
+
+print(average)
+print(f"Average: {average}")
+
+scores = []
+for i in range(3):
+    score = int(input("Score "))
+    scores.append(score)
+
+average2 = sum(scores) / len(scores)
+print(f"Average: {average2}")
+
+names = ["Steve", "Bob", "Dave"]
+name = input("Name: ")
+#for n in names:
+#    if name == n:
+#        print("Found")
+#       break
+if name in names: #Uses linear search
+    print("Found") 
+else:
+    print ("Not Found")
+
+#A LIST of DICTIONARIES
+people = [
+    {"Name": "Bob", "Number": "012"},
+    {"Name": "Dave", "Number": "456"},
+    {"Name": "Steve", "Number": "890"},
+]
+
+name = input("Name: ")
+
+for person in people:
+    if person["Name"] == name:
+        number = person["Number"]
+        print(f"Found: {number}")
+        break
+else:
+    print("Not Found")
+
+people2 = {
+    "Bobby": "Mild",
+    "Bibby": "Spicy",
+    "Blobby": "Hot",
+}
+
+name2 = input("Name2: ")
+
+if name2 in people2:
+    number = people2[name2]
+    print(f"Found: {number}")
+else:
+    print("Not Found")
+
+import sys #To make python worse
+if len(sys.argv) != 2:
+    print("Missing command-line argument")
+    sys.exit(1)
+
+print(f"hello, {sys.argv[1]}")
+sys.exit(0)
